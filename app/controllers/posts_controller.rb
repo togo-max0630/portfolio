276026b686bf116d183e6d5220c44d5c2254e8f9
Togo <a.kirigo.t0630@gmail.com>
2020-06-05 05:40:42 +0000

    [追加]投稿機能・その他

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,36 @@
 class PostsController < ApplicationController
 
 	def index
+		@post = Post.new
 		@posts = Post.all
+		@user = current_user
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def edit
 	end
 
 	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		@post.save
+		redirect_to posts_path
+
 	end
 
 	def update
 	end
 
 	def destroy
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body, :user_id)
 	end
 	
 end
