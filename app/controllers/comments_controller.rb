e794dde940647e1f647cbe49c3f313ec76ec90ad
Togo <a.kirigo.t0630@gmail.com>
2020-06-08 07:00:13 +0000

    [追加]コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,23 @@
 class CommentsController < ApplicationController
 
   def create
+    @post = Post.find(params[:post_id])
+    # buildはnewと同じ
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    render 'index'
   end
   
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render 'index'
   end
   
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_042504) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
-    t.text "comment"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
