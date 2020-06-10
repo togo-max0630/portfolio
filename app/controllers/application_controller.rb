0c180f9f57dcec92686e509f83d285780ddf1ec6
Togo <a.kirigo.t0630@gmail.com>
2020-06-10 13:18:01 +0000

    [変更]ヘッダーcss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    user_path(resource)
+    root_path(resource)
   end
 
   # 新規登録の保存機能
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 	def index
 		@post = Post.new
 		@q = Post.ransack(params[:q])
-		@posts = @q.result(distinct: true).page(params[:page]).per(5)
+		@posts = @q.result(distinct: true).page(params[:page]).per(4)
 		@user = current_user
 	end
 
